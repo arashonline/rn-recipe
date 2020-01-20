88014327b3be74463ad772939dcad6a1b40926a9
rabiei <rabiei@jetsgram.ir>
2020-01-21 02:57:18 +0330

    adding font

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+const [dataLoaded, setDataLoaded] = useState(false);
+
+if (!dataLoaded) {
+  return (
+    <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setDataLoaded(true)}
+      onError={err => console.log(err)}
+    />
+  );
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
