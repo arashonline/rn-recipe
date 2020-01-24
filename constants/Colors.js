0c6d4a7cb354eb73d15062d6992cc9e75965ae78
rabiei <rabiei@jetsgram.ir>
2020-01-25 01:42:20 +0330

    using some color to style header

---
+++ constants/Colors.js
@@ -0,0 +1,5 @@
+export default {
+    primary: '#bdff66',
+    secondary: '#ff8a66',
+    triadic: '#a866ff',
+}
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import { View, Text,FlatList, StyleSheet } from 'react-native';
-import CategoryMealScreen from './CategoryMealsScreen';
-import { CATEGORIES } from '../data/dummy';
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Platform
+} from "react-native";
+import CategoryMealScreen from "./CategoryMealsScreen";
+import { CATEGORIES } from "../data/dummy";
+import Colors from "../constants/Colors";
 
-
-const renderGridItem = (itemData) => {
-  return <View style={styles.gridItem}>
-  <Text>{itemData.item.title}</Text>
-  </View>
-};
 
 const CategoriesScreen = props => {
+  const renderGridItem = itemData => {
+    return (
+      <TouchableOpacity style={styles.gridItem} onPress={()=> {
+        props.navigation.navigate({routeName:'CategoryMeals'});
+      }}>
+        <View >
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  
   return (
     <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   );
+
+  
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories',
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android'? Colors.primary :''
+  },
+  headerTintColor: 'black',
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   gridItem: {
     flex: 1,
