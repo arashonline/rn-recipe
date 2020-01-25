e29b76126ac7f7017f40cfa069b0015e98c6d147
rabiei <rabiei@jetsgram.ir>
2020-01-26 01:18:39 +0330

    added meals added some components

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -10,22 +10,23 @@ import {
 import CategoryMealScreen from "./CategoryMealsScreen";
 import { CATEGORIES } from "../data/dummy";
 import Colors from "../constants/Colors";
+import CategoryGridTile from '../components/CategoryGridTile'
 
 const CategoriesScreen = props => {
   const renderGridItem = itemData => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
-        onPress={() => {
-          props.navigation.navigate({ routeName: "CategoryMeals", params: {
+      <CategoryGridTile 
+      title={itemData.item.title}
+      color={itemData.item.color}
+      onSelect={()=>{
+        props.navigation.navigate({
+          routeName: 'CategoryMeals',
+          params:{
             categoryId: itemData.item.id
-          } });
-        }}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+          }
+        })
+      }}
+      />
     );
   };
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { View, Text,Button, StyleSheet } from 'react-native';
-import { CATEGORIES } from '../data/dummy';
+import { View, Text,FlatList, StyleSheet } from 'react-native';
+import { CATEGORIES , MEALS} from '../data/dummy';
 
 const CategoryMealScreen = props => {
+
+  const renderMealItem = itemData =>{
+    return (
+      <View><Text>{itemData.item.title}</Text></View>
+    );
+  }
+
   const categoryId = props.navigation.getParam('categoryId');
+
+  const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(categoryId) >=0)
+
   const selectedCategory = CATEGORIES.find(cat =>cat.id === categoryId )
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen!</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button title='Meals Detail' onPress={()=>{
-        props.navigation.navigate({
-          routeName:'MealDetail'
-        })
-      }} />
+      <FlatList data={displayedMeals} 
+      renderItem={renderMealItem}
+      />
     </View>
   );
 };
